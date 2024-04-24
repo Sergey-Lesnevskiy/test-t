54814df27a2a84de412493bc86e47b2e825fecde
Lesnevsky-Sergey <78030028+Sergey-Lesnevskiy@users.noreply.github.com>
2024-04-24 21:20:01 +0300

    fix: add second options for fibo

--- nthFibo.js
+++ nthFibo.js
@@ -3,16 +3,14 @@ function nthFibo(index) {
   let now = 0;
   let before = null;
 
-  for (let i = 0; i < index ; i++) {
-  
+  for (let i = 0; i < index; i++) {
     if (!now && i === 1) {
-      now +=1;
+      now += 1;
       before = 0;
     } else if (!now) {
       now = 0;
       before += 1;
-    } 
-    else {
+    } else {
       let c = before + now;
       before = now;
       now = c;
@@ -20,17 +18,23 @@ function nthFibo(index) {
   }
   return now;
 }
+
 console.log(nthFibo(4));
 
 // second option
 
-// const nthFibo2 = (n) => {  
-//   if ( n <= 0) {
-//     return n;
-//   }
-//   // if (n <= 1 ) {
-//   //   return n;
-//   // }
-//   return nthFibo2(n - 1) + nthFibo2(n - 2);
-// };
-// console.log(nthFibo2(4))
+const nthFibo2 = (n) => {
+  if(n < 0){
+    return ["You entered the wrong number, it should be greater than 0"];
+  }
+  if (n == 0) {
+    return [0, 1];
+  } else {
+    const [prev, next] = nthFibo2(n - 1);
+    return [next, prev + next];
+  }
+};
+const fib = (n) => nthFibo2(n - 1)[0];
+
+
+console.log(fib(2));
